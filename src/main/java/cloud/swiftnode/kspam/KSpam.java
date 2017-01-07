8641810195b0913b5394db5099d930123cfc4a8c
Junhyeong Lim <entrypoint_@naver.com>
2017-01-08 05:50:31 +0900

    Add update check feature, Version class, consoleMsg redesign

--- src/main/java/cloud/swiftnode/kspam/KSpam.java
+++ src/main/java/cloud/swiftnode/kspam/KSpam.java
@@ -5,12 +5,15 @@ import cloud.swiftnode.kspam.util.Lang;
 import cloud.swiftnode.kspam.util.Static;
 import cloud.swiftnode.kspam.util.StaticStorage;
 import cloud.swiftnode.kspam.util.URLs;
+import cloud.swiftnode.kspam.util.Version;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URL;
@@ -29,6 +32,7 @@ public class KSpam extends JavaPlugin {
         INSTANCE = this;
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
         cacheInit();
+        updateCheck();
         Static.consoleMsg(Lang.INTRO.builder()
                 .single(Lang.Key.KSPAM_VERSION, Static.getVersion()));
     }
@@ -56,8 +60,9 @@ public class KSpam extends JavaPlugin {
             StaticStorage.cachedSet = (Set<String>) inStream.readObject();
             Static.consoleMsg(Lang.CACHE_COUNT.builder()
                     .prefix().single(Lang.Key.CACHE_COUNT, StaticStorage.cachedSet.size()));
+            throw new Exception("test");
         } catch (Exception ex) {
-            ex.printStackTrace();
+            Static.consoleMsg(ex);
         }
     }
 
@@ -67,7 +72,34 @@ public class KSpam extends JavaPlugin {
             ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream(file));
             outStream.writeObject(StaticStorage.cachedSet);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            Static.consoleMsg(ex);
+        }
+    }
+
+    private void updateCheck() {
+        try {
+            URL url = new URL("https://github.com/EntryPointKR/K-SPAM/releases/latest");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.contains("<span class=\"css-truncate-target\">")) {
+                    continue;
+                }
+                StaticStorage.setNewVer(new Version(
+                        Static.substring(line, "<span class=\"css-truncate-target\">", "</span>")));
+                if (StaticStorage.getCurrVer().beforeEquals(StaticStorage.getNewVer())) {
+                    Static.consoleMsg(
+                            Lang.UPDATE_INFO_NEW.builder().prefix(),
+                            Lang.NEW_VERSION.builder().single(Lang.Key.NEW_VERSION, StaticStorage.getNewVer()).prefix(),
+                            Lang.CURRENT_VERSION.builder().single(Lang.Key.KSPAM_VERSION, StaticStorage.getCurrVer()).prefix()
+                    );
+                } else {
+                    Static.consoleMsg(Lang.UPDATE_INFO_NO.builder().prefix());
+                }
+                return;
+            }
+        } catch (Exception ex) {
+            Static.consoleMsg(ex);
         }
     }
 }
--- src/main/java/cloud/swiftnode/kspam/util/Lang.java
+++ src/main/java/cloud/swiftnode/kspam/util/Lang.java
@@ -30,7 +30,12 @@ public enum Lang {
             " &fPlugin Contributors &eEntryPoint, horyu1234\n" +
             " &fK-SPAM DB Powered By &eSwiftnode\n" +
             " &f버그 제보/건의 &ehttps://github.com/EntryPointKR/K-SPAM/issues\n"),
-    MOTD("&c당신은 K-SPAM 에 의해 차단되었습니다."),;
+    MOTD("&c당신은 K-SPAM 에 의해 차단되었습니다."),
+    UPDATE_INFO_NEW("새 버전이 있습니다."),
+    NEW_VERSION("&e최신버전: &f" + Key.NEW_VERSION),
+    CURRENT_VERSION("&e현재버전: &f" + Key.KSPAM_VERSION),
+    UPDATE_INFO_NO("최신버전입니다."),
+    EXCEPTION("예외가 발생했습니다. 개발자에게 문의해주세요. &bhttps://github.com/EntryPointKR/K-SPAM/issues &e정보: &f" + Key.EXCEPTION_MESSAGE);
     private final String msg;
 
     Lang(String msg) {
@@ -56,7 +61,9 @@ public enum Lang {
         CHECKER_RESULT("checker-result"),
         CACHE_COUNT("cache-count"),
         KSPAM_VERSION("kspam-version"),
-        TIME("time"),;
+        NEW_VERSION("new-version"),
+        TIME("time"),
+        EXCEPTION_MESSAGE("exception-message"),;
         private final String key;
 
         Key(String key) {
