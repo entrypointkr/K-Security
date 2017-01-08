9588be4bcb531633cda99bae76fe5c2b412b5ce4
Junhyeong Lim <entrypoint_@naver.com>
2017-01-08 23:08:46 +0900

    Fix CacheChecker code

--- src/main/java/cloud/swiftnode/kspam/abstraction/checker/CacheChecker.java
+++ src/main/java/cloud/swiftnode/kspam/abstraction/checker/CacheChecker.java
@@ -5,6 +5,10 @@ import cloud.swiftnode.kspam.abstraction.deniable.DeniableInfoAdapter;
 import cloud.swiftnode.kspam.util.StaticStorage;
 import cloud.swiftnode.kspam.util.Tracer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by EntryPoint on 2016-12-30.
  */
@@ -15,10 +19,13 @@ public class CacheChecker extends SpamChecker {
 
     @Override
     public Tracer.Result check() throws Exception {
-        String[] infoArr = new String[]{
-                adapter.getIp(), adapter.getName(), adapter.getUniqueId()
-        };
-        for (String info : infoArr) {
+        List<String> infoList = new ArrayList<>(Arrays.asList(adapter.getIp(), adapter.getName()));
+        try {
+            infoList.add(adapter.getUniqueId());
+        } catch (IllegalStateException ex) {
+            // Ignore
+        }
+        for (String info : infoList) {
             if (StaticStorage.cachedSet.contains(info)) {
                 return Tracer.Result.DENY;
             }
