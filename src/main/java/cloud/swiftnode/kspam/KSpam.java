7839f0fe98449f28d34cfddd1e87b9e2b1cd4311
Junhyeong Lim <entrypoint_@naver.com>
2017-01-10 04:00:03 +0900

    Add Main class

---
+++ src/main/java/cloud/swiftnode/kspam/KSpam.java
@@ -0,0 +1,13 @@
+package cloud.swiftnode.kspam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Junhyeong Lim on 2017-01-10.
+ */
+public class KSpam extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
