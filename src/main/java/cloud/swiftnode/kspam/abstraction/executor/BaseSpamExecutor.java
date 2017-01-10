ce9ff6856e06fb8db94790a79b32594a5a9b0901
Junhyeong Lim <entrypoint_@naver.com>
2017-01-10 17:40:43 +0900

    Add debug message print code

--- src/main/java/cloud/swiftnode/kspam/abstraction/executor/BaseSpamExecutor.java
+++ src/main/java/cloud/swiftnode/kspam/abstraction/executor/BaseSpamExecutor.java
@@ -4,6 +4,8 @@ import cloud.swiftnode.kspam.abstraction.Deniable;
 import cloud.swiftnode.kspam.abstraction.SpamChecker;
 import cloud.swiftnode.kspam.abstraction.SpamExecutor;
 import cloud.swiftnode.kspam.abstraction.SpamProcessor;
+import cloud.swiftnode.kspam.util.Lang;
+import cloud.swiftnode.kspam.util.Static;
 
 /**
  * Created by Junhyeong Lim on 2017-01-10.
@@ -11,13 +13,18 @@ import cloud.swiftnode.kspam.abstraction.SpamProcessor;
 public class BaseSpamExecutor extends SpamExecutor {
     @Override
     public boolean execute(SpamProcessor processor, SpamChecker checker, Deniable deniable) {
-        lastResult = checker.spamCheck();
+        try {
+            lastResult = checker.spamCheck();
+        } catch (Exception ex) {
+            lastResult = SpamChecker.Result.ERROR;
+            Static.consoleMsg(ex);
+        }
         if (lastResult == SpamChecker.Result.FORCE_PASS) {
             return true;
         } else if (lastResult == SpamChecker.Result.DENY) {
             deniable.deny();
         } else if (lastResult == SpamChecker.Result.ERROR) {
-            // TODO: Exception msg
+            Static.consoleMsg(Lang.ERROR.builder().addKey());
         }
         return false;
     }
