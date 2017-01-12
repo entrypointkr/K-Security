1cac52c6f8a105449b9c8ac8138dc7a5fcf5ae44
Junhyeong Lim <entrypoint_@naver.com>
2017-01-12 22:21:38 +0900

    Update info command

--- src/main/java/cloud/swiftnode/kspam/listener/PlayerListener.java
+++ src/main/java/cloud/swiftnode/kspam/listener/PlayerListener.java
@@ -9,6 +9,7 @@ import cloud.swiftnode.kspam.abstraction.processor.SyncLoginProcessor;
 import cloud.swiftnode.kspam.util.Static;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -40,8 +41,15 @@ public class PlayerListener implements Listener {
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onJoin(PlayerJoinEvent e) {
+        /*
+        K-SPAM 은 AGPL 라이선스이며 개발자, 플러그인 정보, 소스 제공이 의무입니다.
+        밑 메세지 전송 코드를 제거 시 법적 조치를 받을 수 있습니다.
+        */
+        e.getPlayer().sendMessage("§c[ K-SPAM ] §f본 서버는 봇 테러 방지 플러그인 §eK-SPAM §f을 사용 중입니다.");
+        e.getPlayer().sendMessage("§c[ K-SPAM ] §f기여자: §eEntryPoint, horyu1234");
+        e.getPlayer().sendMessage("§c[ K-SPAM ] §fhttps://github.com/EntryPointKR/K-SPAM");
         SpamExecutor executor = Static.getDefaultExecutor();
         DeniableInfoAdapter adapter = new DeniableInfoAdapter(true, e.getPlayer());
         SpamProcessor processor = new SyncJoinProcessor(executor, adapter);
