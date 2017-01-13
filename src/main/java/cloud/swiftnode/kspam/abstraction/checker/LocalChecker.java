63ba7f6584a27cfa5e9c4e16c27060fd233e44ae
Junhyeong Lim <entrypoint_@naver.com>
2017-01-14 01:09:16 +0900

    Fix LocalChecker

--- src/main/java/cloud/swiftnode/kspam/abstraction/checker/LocalChecker.java
+++ src/main/java/cloud/swiftnode/kspam/abstraction/checker/LocalChecker.java
@@ -2,6 +2,7 @@ package cloud.swiftnode.kspam.abstraction.checker;
 
 import cloud.swiftnode.kspam.abstraction.Info;
 import cloud.swiftnode.kspam.abstraction.SpamChecker;
+import org.bukkit.entity.Player;
 
 /**
  * Created by Junhyeong Lim on 2017-01-11.
@@ -14,8 +15,9 @@ public class LocalChecker extends SpamChecker {
     @Override
     public Result spamCheck() throws Exception {
         String ip = lastInfo = info.getIp();
+        Player player = info.getPlayer();
         if (ip.contains("192.168.") || ip.contains("127.0.")
-                || (info.getPlayer() != null && info.getPlayer().isOp())) {
+                || player != null && (player.isOp() || player.hasPlayedBefore())) {
             return Result.FORCE_PASS;
         }
         return Result.PASS;
