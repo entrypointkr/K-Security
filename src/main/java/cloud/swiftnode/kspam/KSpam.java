e5cc442756ae7e975c4a3642b91f536736a6974d
ldmsys <ldmldm05@gmail.com>
2017-01-14 18:22:57 +0900

    break 구문과 ! 없애기
    이러면 될까요..?

--- src/main/java/cloud/swiftnode/kspam/KSpam.java
+++ src/main/java/cloud/swiftnode/kspam/KSpam.java
@@ -64,12 +64,11 @@ public class KSpam extends JavaPlugin {
         saveConfig();
         new CacheSaveProcessor().process();
         boolean sws = getConfig().getBoolean("shutdownwithserver", false);
-        if (!sws) {
-        break;
+        if (sws) {
+            //OP가 플러그인 종료후 봇테러 날리는일 방지
+            System.out.println("경고! K-SPAM 플러그인이 종료되었습니다. config.yml 정책에 의해 서버와 함께 종료됩니다.");
+            Bukkit.shutdown();
         }
-        //OP가 플러그인 종료후 봇테러 날리는일 방지
-        System.out.println("경고! K-SPAM 플러그인이 종료되었습니다. config.yml 정책에 의해 서버와 함께 종료됩니다.");
-        Bukkit.shutdown();
     }
 
     @Override
