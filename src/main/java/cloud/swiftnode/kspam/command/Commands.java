9ac26eb15a3955af170c213e627c26cb1ca3cda7
ldmsys <ldmldm05@gmail.com>
2017-01-19 18:54:50 +0900

    Update Commands.java

--- src/main/java/cloud/swiftnode/kspam/command/Commands.java
+++ src/main/java/cloud/swiftnode/kspam/command/Commands.java
@@ -45,7 +45,7 @@ public class Commands implements CommandExecutor {
                      밑 메세지 전송 코드를 제거 시 법적 책임을 물을 수 있습니다.
                      본 프로젝트에 기여했을 경우 밑 메세지에 자신의 닉네임을 추가할 수 있습니다.
                      */
-                    sender.sendMessage(Lang.LAW_INFO);
+                    sender.sendMessage(Lang.LAW_INFO.builder().prefix().build());
                     sender.sendMessage(Lang.NEW_VERSION.builder().single(Lang.Key.NEW_VERSION, StaticStorage.getNewVer()).prefix().build());
                     sender.sendMessage(Lang.CURRENT_VERSION.builder().single(Lang.Key.KSPAM_VERSION, StaticStorage.getCurrVer()).prefix().build());
                     sender.sendMessage(Lang.PREFIX + String.valueOf(StaticStorage.cachedSet.size()));
