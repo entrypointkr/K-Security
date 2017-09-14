ca4042594d0328d6d5df70e6c21c484c7abcf9a6
Junhyeong Lim <entrypoint_@naver.com>
2017-09-15 05:59:33 +0900

    Fix GUI error

--- src/main/java/cloud/swiftnode/ksecurity/module/kgui/KFX.java
+++ src/main/java/cloud/swiftnode/ksecurity/module/kgui/KFX.java
@@ -1,15 +1,11 @@
 package cloud.swiftnode.ksecurity.module.kgui;
 
-import cloud.swiftnode.ksecurity.module.kgui.abstraction.gui.KAlert;
-import cloud.swiftnode.ksecurity.util.Lang;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 /**
  * Created by Junhyeong Lim on 2017-02-04.
@@ -29,21 +25,16 @@ public class KFX extends Application {
         stage = primaryStage;
         FXMLLoader.setDefaultClassLoader(getClass().getClassLoader());
         Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
-        primaryStage.setOnCloseRequest(event -> hideStage(primaryStage));
+        primaryStage.setOnCloseRequest(event -> {
+            primaryStage.setIconified(true);
+            event.consume();
+        });
         primaryStage.setTitle("K-Security 이벤트 로그");
         primaryStage.setScene(new Scene(root, width, height));
         primaryStage.setMinWidth(width);
         primaryStage.setMinHeight(height);
         primaryStage.show();
         Platform.setImplicitExit(false);
-    }
-
-    private void hideStage(Stage stage) {
-        if (SystemTray.isSupported()) {
-            stage.hide();
-            new KAlert().setContextText(Lang.CLOSE_ALERT.builder().flatBuild())
-                    .show();
-        }
     }
 
     public static Stage getStage() {
